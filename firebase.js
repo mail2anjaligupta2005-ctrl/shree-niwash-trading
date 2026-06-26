266b0050c4f68af5d51c9a533d29541b66c952f4
Anjali Gupta <mail2anjaligupta2005@gmail.com>
2026-06-26 14:58:30 +0530

    Updated firebase for Render

--- firebase.js
+++ firebase.js
@@ -1,9 +1,15 @@
 const admin = require("firebase-admin");
 
-const serviceAccount = require("./firebase-key.json");
+let serviceAccount;
+
+if (process.env.FIREBASE_SERVICE_ACCOUNT) {
+    serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT);
+} else {
+    serviceAccount = require("./firebase-key.json");
+}
 
 admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount)
+    credential: admin.credential.cert(serviceAccount)
 });
 
 const db = admin.firestore();
